3cd4160346b80df6a2fa60704b85d251479e5280
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-03-04 10:21:00 -0600

    credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  
   validates :email, presence: true, uniqueness: true
   validates :zip_code, presence: true
   validates :password, presence: true
